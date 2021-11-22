62621203db8af2587b8a484b2d3d60d632e809ab
Sany <Sany@minutemedia.com>
2021-11-22 20:27:10 +0200

    solution for exercise1

--- coolfacts/entrypoint/main.go
+++ coolfacts/entrypoint/main.go
@@ -2,8 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("Welcome to the go-workshop, hope you will have fun!!")
+	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprint(w, "PONG")
+		if err != nil {
+			errMessage := fmt.Sprintf("error writing response: %v", err)
+			http.Error(w, errMessage, http.StatusInternalServerError)
+		}
+	})
+
+	log.Println("starting server")
+	err := http.ListenAndServe(":9002", nil)
+	if err != nil {
+		fmt.Println("error listening server: " ,err)
+	}
 }
+
+
