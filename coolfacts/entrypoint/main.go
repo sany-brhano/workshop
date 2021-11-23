e7fcdcb6d937ea2a55547fccd6dda39dda6a8e59
Sany <Sany@minutemedia.com>
2021-11-23 10:40:45 +0200

    solution for exercise3

--- coolfacts/entrypoint/main.go
+++ coolfacts/entrypoint/main.go
@@ -3,82 +3,34 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"html/template"
-	"io/ioutil"
 	"log"
 	"net/http"
 )
 
-var newsTemplate = `<!DOCTYPE html>
-<html>
-  <head><style>/* copy coolfacts/styles.css for some color 🎨*/</style></head>
-  <body>
-  <h1>Facts List</h1>
-  <div>
-    {{ range . }}
-       <article>
-            <h3>{{.Description}}</h3>
-            <img src="{{.Image}}" width="80%" />
-       </article>
-    {{ end }}
-  <div>
-  </body>
-</html>`
-
 func main() {
-	factRepo := repo{
+	factsRepo := repo{
 		facts: []fact{
 			{Image: "pic1", Description: "DOGS PIC"},
-			{Image: "pic2", Description: "CATS PIC"},
 		},
 	}
+	factsRepo.add(fact{
+		Image:       "https://images2.minutemediacdn.com/image/upload/v1556645500/shape/cover/entertainment/D5aliXvWsAEcYoK-fe997566220c082b98030508e654948e.jpg",
+		Description: "Did you know sonic is a hedgehog?!",
+	})
 
-	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		_, err := fmt.Fprint(w, "PONG")
+	http.HandleFunc("/facts", func(w http.ResponseWriter, r *http.Request) {
+		allFacts := factsRepo.getAll()
+		bs, err := json.Marshal(allFacts)
+		if err != nil {
+			errMessage := fmt.Sprintf(" marshal error")
+			http.Error(w, errMessage, http.StatusInternalServerError)
+		}
+		_, err = w.Write(bs)
 		if err != nil {
 			errMessage := fmt.Sprintf("error writing response: %v", err)
 			http.Error(w, errMessage, http.StatusInternalServerError)
 		}
-	})
 
-	http.HandleFunc("/facts", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Add("Content-Type", "text/html")
-		if r.Method == http.MethodGet {
-			tmpl, err := template.New("facts").Parse(newsTemplate)
-			if err != nil {
-				return
-			}
-			facts := factRepo.getAll()
-			err = tmpl.Execute(w, facts)
-			if err != nil {
-				errMessage := fmt.Sprintf("error executing html: %v", err)
-				http.Error(w, errMessage, http.StatusInternalServerError)
-			}
-		}
-
-		if r.Method == http.MethodPost {
-			body, err := ioutil.ReadAll(r.Body)
-			if err != nil {
-				errMessage := fmt.Sprintf("error reading response: %v", err)
-				http.Error(w, errMessage, http.StatusInternalServerError)
-			}
-			var factPost fact
-			err = json.Unmarshal(body, &factPost)
-			if err != nil {
-				fmt.Println("error Unmarshallling json slice:", err)
-			}
-
-			f := fact{
-				Image:       factPost.Image,
-				Description: factPost.Description,
-			}
-			factRepo.add(f)
-
-			_, wErr := w.Write([]byte("SUCCESS"))
-			if wErr != nil {
-				fmt.Println("error writing SUCCESS: ", wErr)
-			}
-		}
 	})
 
 	log.Println("starting server")
